d226ffab22c8082c265df0d2369cebbde94f2fd6
Волков Алекесей <volkov@ulstu.net>
2014-04-26 15:08:22 +0400

    Added Angular

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function change(hash){
+/*function change(hash){
 	var arr = hash.split('/');
 	var html = "helo";
 
@@ -6,7 +6,7 @@ function change(hash){
 		case 'compose':
 		html = "<div class=\"input-group\">\
   					<span class=\"input-group-addon\">Кому:</span>\
-  					<input type=\"text\" class=\"form-control\">\
+  					<input type=\"text\" class=\"form-control\" ng-model=\"yourName\">\
 				</div>"+
 				//SUBJECT
 				"<div class=\"input-group\">\
@@ -19,11 +19,27 @@ function change(hash){
 				</div>";
 		break;
 	}
-	$(".message").html(html);
+	//$("#main").html(html);
+}*/
+
+function EmailController($scope) {
+    $scope.isPopupVisible = false;
+
+    $scope.showPopup = function (email) {
+    $scope.isPopupVisible = true;
+    $scope.selectedEmail = email;
+    console.log($scope.selectedEmail);
+};
+
+    $scope.emails = [
+       { from: 'John', subject: 'I love angular', date: 'Jan 1' },
+       { from: 'Jack', subject: 'Angular and I are just friends', date: 'Feb 15' },
+       { from: 'Ember', subject: 'I hate you Angular!', date: 'Dec 8' }
+    ];
 }
 
 $(document).ready(function(){
-	var hash = window.location.hash.slice(3);
+	/*var hash = window.location.hash.slice(3);
 	change(hash);
 	// on Hash change
 	$(window).on('hashchange',function(){ 
@@ -36,5 +52,5 @@ $(document).ready(function(){
 		var compose_hash = "#!/compose"
 		window.location.hash = compose_hash;
 		change(hash);
-	});
+	});*/
 });
