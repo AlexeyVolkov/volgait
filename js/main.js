cc0583c97b2774f8b7c20e088db4e4448f3eb25a
Волков Алекесей <volkov@ulstu.net>
2014-04-26 11:54:19 +0400

    First

---
+++ js/main.js
@@ -0,0 +1,9 @@
+require(["jquery.min"], function(util) {
+    console.log("jquery.min.js");
+});
+require(["bootstrap.min"], function(util) {
+    console.log("bootstrap.min.js");
+});
+require(["less-1.7.0.min"], function(util) {
+    console.log("less-1.7.0.min.js");
+});
