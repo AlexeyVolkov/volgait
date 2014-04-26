b851e5156f8c00cbe398cb244738b31d7af2690b
Волков Алекесей <volkov@ulstu.net>
2014-04-26 12:57:08 +0400

    Hash and compose

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
-$(document).ready(function(){
-	var hash = window.location.hash.slice(2);
+function change(hash){
+	var arr = hash.split('/');
+	var html = "helo";
 
+	switch(arr[0]){
+		case 'compose':
+		html = "<div class=\"input-group\">\
+  					<span class=\"input-group-addon\">Кому:</span>\
+  					<input type=\"text\" class=\"form-control\">\
+				</div>"+
+				//SUBJECT
+				"<div class=\"input-group\">\
+  					<span class=\"input-group-addon\">Тема:</span>\
+  					<input type=\"text\" class=\"form-control\">\
+				</div>"+
+				//TEXT
+				"<div class=\"input-group\">\
+  					<textarea class=\"form-control\" placeholder=\"Текст письма\"></textarea>\
+				</div>";
+		break;
+	}
+	$(".message").html(html);
+}
+
+$(document).ready(function(){
+	var hash = window.location.hash.slice(3);
+	change(hash);
 	// on Hash change
 	$(window).on('hashchange',function(){ 
-    	hash = location.hash.slice(2);
-    	console.log(hash);
+    	hash = location.hash.slice(3);
+    	change(hash);
 	});
 
-	console.log(hash);
+	//compose button
+	$("#compose").click(function(){
+		var compose_hash = "#!/compose"
+		window.location.hash = compose_hash;
+		change(hash);
+	});
 });
